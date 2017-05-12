d6ef43a28f7910f81983ea15e1257b2174f57b9f
Gustaf Ekeberg <firman@gustafekeberg.se>
2017-05-13 00:45:52 +0200

    Organized code

--- src/main/resources/site/filters/liveReload.js
+++ src/main/resources/site/filters/liveReload.js
@@ -1,37 +1,38 @@
 var libs = {
 	portal: require('/lib/xp/portal'),
-	content: require('/lib/xp/content'),
-	thymeleaf: require('/lib/xp/thymeleaf'),
 };
 
 exports.responseFilter = function (req, res) {
 
 	var config = libs.portal.getSiteConfig();
-	var enable = config.enable;
+	var enabled = config.enable;
 	var mode = req.mode;
-	var port = config.port ? config.port : "35729";
 
 	// Only run if enabled and not in edit mode
-	if (!enable || mode == 'edit')
+	if (enabled && mode !== 'edit')
+		return filter (req, res, config);
+	else
 		return res;
-	
+};
+
+function filter (req, res, config) {
+	var port = config.port ? config.port : "35729";
 	var liveReloadScript = "<script>document.write('<script src=\"http://'" +
 	"+ location.host.split(':')[0]" +
 	"+ ':" + port + "/livereload.js\"></'" +
 	"+ 'script>')</script>";
 	
 	var bodyEnd = res.pageContributions.bodyEnd;
-	    if (!bodyEnd) {
-        res.pageContributions.bodyEnd = [];
-    }
-    if (typeof bodyEnd == 'string') {
-        res.pageContributions.bodyEnd = [ bodyEnd ];
-    }
-    res.pageContributions.bodyEnd.push(liveReloadScript);
+  if (!bodyEnd) {
+    res.pageContributions.bodyEnd = [];
+  }
+  if (typeof bodyEnd == 'string') {
+      res.pageContributions.bodyEnd = [ bodyEnd ];
+  }
+  res.pageContributions.bodyEnd.push(liveReloadScript);
 
-    if (req.params.debug === 'true') {
-        res.applyFilters = false; // skip other filters
-    }
-
-    return res;
-};
+  if (req.params.debug === 'true') {
+      res.applyFilters = false; // skip other filters
+  }
+  return res;
+}
